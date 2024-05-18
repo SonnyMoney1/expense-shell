0356c201688305aeda826247c051a353c271f00d
new commit <ebhos4real@yahoo.com>
2024-05-17 20:10:38 -0400

    added backend service error adding mysql Added mysql
    This to ensured that the necessary changes are made
    
    This too is added, what's next
    I just added to my repos
    I have added additional info

--- backend.sh
+++ backend.sh
@@ -6,7 +6,6 @@ mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
 unzip /tmp/backend.zip
-npm install
 
 dnf install mysql -y
 mysql -h 172.31.19.91 -uroot -pExpenseApp@1 < /app/schema/backend.sql
